7dab9f86155186df16da5d502737af29dc4dd3b7
estheerzoca <estherzocasilva@gmail.com>
2025-06-27 16:28:16 -0400

    Create esfera.php

---
+++ esfera.php
@@ -0,0 +1,12 @@
+<?php
+define("PI", 3.141592);
+
+$raio = 5; // Você pode mudar o valor do raio aqui
+
+$area = 4 * PI * $raio * $raio;
+$volume = (4 / 3) * PI * $raio * $raio * $raio;
+
+echo "Raio: $raio <br>";
+echo "Área da esfera: $area <br>";
+echo "Volume da esfera: $volume <br>";
+?>
